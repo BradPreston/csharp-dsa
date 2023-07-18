d6cb40ed0aa6f5220f88111b05630033bca84827
Brad Preston <bap5393@gmail.com>
2023-07-18 07:30:33 -0400

    Add MaxBinaryHeap to DataStructures assembly

---
+++ DataStructures/MaxBinaryHeap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class MaxBinaryHeap<T>
+    {
+        private List<T> values = new List<T>();
+    }
+}

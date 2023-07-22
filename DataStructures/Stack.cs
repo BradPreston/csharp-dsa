c6863a624029cbe5cab1f2f9bc02e09d2c01765c
Brad Preston <bap5393@gmail.com>
2023-07-22 12:18:41 -0400

    Add First, Last, and Size to Stack

--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -8,10 +8,14 @@ namespace DataStructures
 {
     internal class Stack<T>
     {
+        private Node? First = null;
+        private Node? Last = null;
+        internal int Size = 0; 
+
         private class Node
         {
             internal T Value;
-            internal Node? Next;
+            internal Node? Next = null;
 
             internal Node(T Value)
             {

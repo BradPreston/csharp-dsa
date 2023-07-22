be3ef2474fe27adcccebf80a972baee5390ea6ac
Brad Preston <bap5393@gmail.com>
2023-07-22 12:33:23 -0400

    Add comments to Stack

--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
+    /// <summary>
+    /// Stack implements a last in-first out style list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal class Stack<T>
     {
         private Node? First = null;
         private Node? Last = null;
         internal int Size = 0; 
 
+        /// <summary>
+        /// Node represents a node in the Stack.
+        /// </summary>
         private class Node
         {
             internal T Value;
@@ -23,6 +30,10 @@ namespace DataStructures
             }
         }
 
+        /// <summary>
+        /// Push adds a node to the beginning of the stack.
+        /// </summary>
+        /// <param name="Value"></param>
         internal void Push(T Value)
         {
             Node newNode = new Node(Value);
@@ -41,6 +52,10 @@ namespace DataStructures
             Size++;
         }
 
+        /// <summary>
+        /// Pop removes the first node from the stack.
+        /// </summary>
+        /// <returns>Returns the value of the removed node; or the default of <c>T</c> if empty.</returns>
         internal T? Pop()
         {
             if (First == null) return default;

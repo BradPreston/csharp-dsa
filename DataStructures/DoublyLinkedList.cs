35858477af4cb9db77c9a0aeb2b8486bf47fc03f
Brad Preston <bap5393@gmail.com>
2023-07-24 07:22:19 -0400

    Add DoublyLinkedList to DataStructures

---
+++ DataStructures/DoublyLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class DoublyLinkedList<T>
+    {
+        private Node? _head = null;
+        private Node? _tail = null;
+        private int _length = 0;
+
+        private class Node
+        {
+            internal T Value;
+            internal Node? Next = null;
+            internal Node? Prev = null;
+            internal Node(T value)
+            {
+                this.Value = value;
+            }
+        }
+    }
+}

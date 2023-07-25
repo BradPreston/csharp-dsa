8d18f9d2e9e87d5a4b1d2f54c75b1d86c7f8dcd5
bradpreston <bap5393@gmail.com>
2023-07-25 16:28:54 -0400

    Add RemoveEdge method to Graph

--- DataStructures/Graph.cs
+++ DataStructures/Graph.cs
@@ -26,5 +26,17 @@
             vertex1Array?.Add(vertex2);
             vertex2Array?.Add(vertex1);
         }
+
+        internal void RemoveEdge(T vertex1, T vertex2)
+        {
+            if (!_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
+                throw new KeyNotFoundException($"Key: \"{vertex1}\" was not found.");
+            
+            if (!_adjacencyList.TryGetValue(vertex2, out List<T>? vertex2Array))
+                throw new KeyNotFoundException($"Key: \"{vertex2}\" was not found.");
+
+            vertex1Array?.Remove(vertex2);
+            vertex2Array?.Remove(vertex1);
+        }
     }
 }

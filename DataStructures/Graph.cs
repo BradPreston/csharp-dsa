48aa588d4ff16ab883bb5249eee4d750582db71e
Brad Preston <bap5393@gmail.com>
2023-07-25 17:02:00 -0400

    Add comments to Graph

--- DataStructures/Graph.cs
+++ DataStructures/Graph.cs
@@ -5,6 +5,12 @@ namespace DataStructures
     internal class Graph<T> where T : notnull
     {
         private readonly Dictionary<T, List<T>> _adjacencyList = new Dictionary<T, List<T>>();
+        
+        /// <summary>
+        /// Adds a vertex to the Graph.
+        /// </summary>
+        /// <param name="key">The key to add to the Graph.</param>
+        /// <exception cref="ArgumentException">The key already exists.</exception>
         internal void AddVertex(T key)
         {
             if (!_adjacencyList.TryAdd(key, new List<T>()))
@@ -16,7 +22,7 @@ namespace DataStructures
         /// </summary>
         /// <param name="vertex1">The first vertex.</param>
         /// <param name="vertex2">The second vertex.</param>
-        /// <exception cref="KeyNotFoundException"><c>vertex1</c> or <c>vertex2</c> were not found as keys.</exception>
+        /// <exception cref="KeyNotFoundException">Either <c>vertex1</c> or <c>vertex2</c> were not found as a key.</exception>
         internal void AddEdge(T vertex1, T vertex2)
         {
             if (!_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
@@ -29,6 +35,13 @@ namespace DataStructures
             vertex2Array?.Add(vertex1);
         }
 
+        /// <summary>
+        /// Removes an edge between two vertices.
+        /// </summary>
+        /// <param name="vertex1">The first vertex.</param>
+        /// <param name="vertex2">The second vertex.</param>
+        /// <exception cref="KeyNotFoundException"><c>vertex1</c> or <c>vertex2</c> were not found as keys.</exception>
+        /// <exception cref="DataException">No edge between <c>vertex1</c> or <c>vertex2</c>.</exception>
         internal void RemoveEdge(T vertex1, T vertex2)
         {
             if (!_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
@@ -44,6 +57,11 @@ namespace DataStructures
             vertex2Array?.Remove(vertex1);
         }
 
+        /// <summary>
+        /// Removes a vertex from the Graph.
+        /// </summary>
+        /// <param name="vertex">The key to remove.</param>
+        /// <exception cref="KeyNotFoundException">The key does not exist.</exception>
         internal void RemoveVertex(T vertex)
         {
             if (!_adjacencyList.ContainsKey(vertex))

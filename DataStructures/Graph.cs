b51e4aae29c8a4414ef6924af35bc70d3ee6a6f1
Brad Preston <bap5393@gmail.com>
2023-07-25 08:03:02 -0400

    Add AddEdge as a method to Graph

--- DataStructures/Graph.cs
+++ DataStructures/Graph.cs
@@ -2,12 +2,39 @@
 {
     internal class Graph<T> where T : notnull
     {
-        private readonly Dictionary<T, T[]> _adjacencyList = new Dictionary<T, T[]>();
+        private readonly Dictionary<T, List<T>> _adjacencyList = new Dictionary<T, List<T>>();
         internal void AddVertex(T key)
         {
             // TryAdd doesn't add a new key if an exact match key exists.
             // returns true or false instead of throwing an exception
-            _adjacencyList.TryAdd(key, Array.Empty<T>());
+            _adjacencyList.TryAdd(key, new List<T>());
+        }
+
+        /// <summary>
+        /// Adds a two way connection between two vertices.
+        /// </summary>
+        /// <param name="vertex1">The first vertex.</param>
+        /// <param name="vertex2">The second vertex.</param>
+        /// <exception cref="KeyNotFoundException"><c>vertex1</c> or <c>vertex2</c> were not found as keys.</exception>
+        internal void AddEdge(T vertex1, T vertex2)
+        {
+            if (_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
+            {
+                vertex1Array?.Add(vertex2);
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Key: \"{vertex1}\" was not found.");
+            }
+
+            if (_adjacencyList.TryGetValue(vertex2, out List<T>? vertex2Array))
+            {
+                vertex2Array?.Add(vertex1);
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Key: \"{vertex2}\" was not found.");
+            }
         }
     }
 }

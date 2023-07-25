22bb1d38b31b50b5372f91d6604bbf7679756de9
Brad Preston <bap5393@gmail.com>
2023-07-25 07:30:12 -0400

    Add _adjacencyList and AddVertex method to Graph

--- DataStructures/Graph.cs
+++ DataStructures/Graph.cs
@@ -1,7 +1,11 @@
 ﻿namespace DataStructures
 {
-    internal class Graph<T>
+    internal class Graph<T> where T : notnull
     {
-    
+        private readonly Dictionary<T, T[]> _adjacencyList = new Dictionary<T, T[]>();
+        internal void AddVertex(T key)
+        {
+            _adjacencyList.Add(key, Array.Empty<T>());
+        }
     }
 }

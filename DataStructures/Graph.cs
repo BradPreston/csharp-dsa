daca7c059164471cda9777ec94ddb1187740ae09
Brad Preston <bap5393@gmail.com>
2023-07-27 18:07:53 -0400

    Setup Graph to take weights to edges

--- DataStructures/Graph.cs
+++ DataStructures/Graph.cs
@@ -4,7 +4,7 @@ namespace DataStructures
 {
     internal class Graph<T> where T : notnull
     {
-        private readonly Dictionary<T, List<T>> _adjacencyList = new Dictionary<T, List<T>>();
+        private readonly Dictionary<T, List<object>> _adjacencyList = new Dictionary<T, List<object>>();
         
         /// <summary>
         /// Adds a vertex to the Graph.
@@ -13,7 +13,7 @@ namespace DataStructures
         /// <exception cref="ArgumentException">The key already exists.</exception>
         internal void AddVertex(T key)
         {
-            if (!_adjacencyList.TryAdd(key, new List<T>()))
+            if (!_adjacencyList.TryAdd(key, new List<object>()))
                 throw new ArgumentException($"Key: \"{key}\" already exists.");
         }
 
@@ -23,18 +23,27 @@ namespace DataStructures
         /// <param name="vertex1">The first vertex.</param>
         /// <param name="vertex2">The second vertex.</param>
         /// <exception cref="KeyNotFoundException">Either <c>vertex1</c> or <c>vertex2</c> were not found as a key.</exception>
-        internal void AddEdge(T vertex1, T vertex2)
+        internal void AddEdge(T vertex1, T vertex2, int weight = 0)
         {
-            if (!_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
+            if (!_adjacencyList.TryGetValue(vertex1, out List<object>? vertex1Array))
                 throw new KeyNotFoundException($"Key: \"{vertex1}\" was not found.");
             
-            if (!_adjacencyList.TryGetValue(vertex2, out List<T>? vertex2Array))
+            if (!_adjacencyList.TryGetValue(vertex2, out List<object>? vertex2Array))
                 throw new KeyNotFoundException($"Key: \"{vertex2}\" was not found.");
 
             if (vertex1Array?.IndexOf(vertex2) > 0 || vertex2Array?.IndexOf(vertex1) > 0) return;
             
-            vertex1Array?.Add(vertex2);
-            vertex2Array?.Add(vertex1);
+            if (weight == 0)
+            {
+                vertex1Array?.Add(vertex2);
+                vertex2Array?.Add(vertex1);
+            }
+            else
+            {
+                vertex1Array?.Add(new KeyValuePair<T, int>(vertex2, weight));
+                vertex2Array?.Add(new KeyValuePair<T, int>(vertex1, weight));
+            }
+            
         }
 
         /// <summary>
@@ -46,10 +55,10 @@ namespace DataStructures
         /// <exception cref="DataException">No edge between <c>vertex1</c> or <c>vertex2</c>.</exception>
         internal void RemoveEdge(T vertex1, T vertex2)
         {
-            if (!_adjacencyList.TryGetValue(vertex1, out List<T>? vertex1Array))
+            if (!_adjacencyList.TryGetValue(vertex1, out List<object>? vertex1Array))
                 throw new KeyNotFoundException($"Key: \"{vertex1}\" was not found.");
             
-            if (!_adjacencyList.TryGetValue(vertex2, out List<T>? vertex2Array))
+            if (!_adjacencyList.TryGetValue(vertex2, out List<object>? vertex2Array))
                 throw new KeyNotFoundException($"Key: \"{vertex2}\" was not found.");
 
             if (vertex1Array?.IndexOf(vertex2) < 0 || vertex2Array?.IndexOf(vertex1) < 0)
@@ -69,7 +78,7 @@ namespace DataStructures
             if (!_adjacencyList.ContainsKey(vertex))
                 throw new KeyNotFoundException($"Key: \"{vertex}\" was not found.");
             
-            foreach (List<T> value in _adjacencyList.Values)
+            foreach (List<object> value in _adjacencyList.Values)
             {
                 value.Remove(vertex);
             }
@@ -91,7 +100,7 @@ namespace DataStructures
         {
             visited.Add(vertex, true);
             results.Add(vertex);
-            List<T>? values = _adjacencyList.GetValueOrDefault(vertex);
+            List<object>? values = _adjacencyList.GetValueOrDefault(vertex);
             
             if (values == default) return;
             
@@ -119,7 +128,7 @@ namespace DataStructures
                 
                 results.Add(popped);
 
-                List<T>? neighbors = _adjacencyList.GetValueOrDefault(popped);
+                List<object>? neighbors = _adjacencyList.GetValueOrDefault(popped);
 
                 if (neighbors == null) continue;
 
@@ -150,7 +159,7 @@ namespace DataStructures
                 
                 results.Add(currentVertex);
                 
-                List<T>? neighbors = _adjacencyList.GetValueOrDefault(currentVertex);
+                List<object>? neighbors = _adjacencyList.GetValueOrDefault(currentVertex);
 
                 if (neighbors == null) continue;
                 
